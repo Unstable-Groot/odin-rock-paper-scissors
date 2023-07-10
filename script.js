886b40056b29946b3934cfda927600b852f51ce7
Unstable Groot <115673057+UnstableGrooot@users.noreply.github.com>
2023-07-10 17:59:36 -0400

    Console based Rock-Paper-Scissors implemented

---
+++ script.js
@@ -0,0 +1,73 @@
+function getComputerChoice() {
+    const rand = Math.floor(Math.random() * 3);
+
+    if (rand == 0) {
+        return "Rock";
+    }
+    else if (rand == 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "You Lose! Paper covers Rock";
+        }
+        else if (computerSelection == "scissors") {
+            return "You Win! Rock beat scissors";
+        }
+    }
+    else if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            return "You Lose! Scissors cuts Paper";
+        }
+        else if (computerSelection == "rock") {
+            return "You Win! Paper covers Rock";
+        }
+    }
+    else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            return "You Lose! Rock beat scissors";
+        }
+        else if (computerSelection == "paper") {
+            return "You Win! Scissors cuts Paper";
+        }
+    }
+    else {
+        return "Not an option";
+    }
+
+    return "Tie! No one wins.";
+}
+
+function game() {
+    let pseclection = prompt("Please enter your choice (rock, paper, scissors)");
+    let cselection = getComputerChoice();
+    console.log(playRound(pseclection,cselection));
+
+    pseclection = prompt("Please enter your choice (rock, paper, scissors)");
+    cselection = getComputerChoice();
+    console.log(playRound(pseclection,cselection));
+
+    pseclection = prompt("Please enter your choice (rock, paper, scissors)");
+    cselection = getComputerChoice();
+    console.log(playRound(pseclection,cselection));
+
+    pseclection = prompt("Please enter your choice (rock, paper, scissors)");
+    cselection = getComputerChoice();
+    console.log(playRound(pseclection,cselection));
+
+    pseclection = prompt("Please enter your choice (rock, paper, scissors)");
+    cselection = getComputerChoice();
+    console.log(playRound(pseclection,cselection));
+
+}
+
+game();
